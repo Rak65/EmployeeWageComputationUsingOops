6ba5ab24ca2313232dfaa3d30c29304d5dd2d4a9
Rak65 <rakeshp6505@gmail.com>
2023-05-12 21:36:34 +0530

    Ability to save the Total wage for Each Company

---
+++ EmployeeWageComputationUsingOops/EmpWageBuilderObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationUsingOops
+{
+    public class EmpWageBuilderObject
+    {
+        const int IS_FUL_TIME = 1;
+        const int IS_PART_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int dayHour = 0;
+            int totalHours = 0;
+            int totalWorkingDays = 0;
+            while (totalHours < this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FUL_TIME:
+                        dayHour = 8;
+                        break;
+                    case IS_PART_TIME:
+                        dayHour = 4;
+                        break;
+                    default:
+                        dayHour = 0;
+                        break;
+
+                }
+                totalHours += dayHour;
+                Console.WriteLine("Day# " + totalWorkingDays + " Employee hours = " + dayHour);
+            }
+            int totalEmpWage = totalHours * this.empRatePerHour;
+            Console.WriteLine("Total Employee wage for company = " + company + " is " + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Employee wage for company = " + company + " is " + totalEmpWage;
+        }
+    }
+}
--- EmployeeWageComputationUsingOops/Program.cs
+++ EmployeeWageComputationUsingOops/Program.cs
@@ -6,20 +6,16 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputationUsingOops
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Company name : ");
-            String company = Console.ReadLine();
-            Console.WriteLine("Enter Employee rate per hour : ");
-            int EMP_RATE_PER_HOUR = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number of working days :");
-            int NO_OF_WORKING_DAYS = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter maximum working hour in a month :");
-            int MAX_WORKING_HOUR = Convert.ToInt32(Console.ReadLine());
-            CheckEmp compute = new CheckEmp();
-            compute.ComputeEmpWage(company, EMP_RATE_PER_HOUR, NO_OF_WORKING_DAYS, MAX_WORKING_HOUR);
+            EmpWageBuilderObject dmart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dmart.computeEmpWage();
+            Console.WriteLine(dmart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
             Console.ReadKey(); ;
         }
     }
